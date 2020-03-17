c48c9e08e8062eef2317ec3e0505e563448aa69c
azorzovilis <alexzorzov@gmail.com>
2020-03-17 23:47:38 +0000

    Added Berlin clock component

--- BerlinClockClient/src/app/app.module.ts
+++ BerlinClockClient/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BerlinClockComponent } from './clock/berlin-clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BerlinClockComponent
   ],
   imports: [
     BrowserModule
