01b0e546fc372544aa482935adbaf196061087a7
eliza.konst <eliza.konst@live.com>
2020-03-18 21:48:02 +0000

    hard coded html

--- BerlinClockClient/src/app/clock/berlin-clock.component.ts
+++ BerlinClockClient/src/app/clock/berlin-clock.component.ts
@@ -1,14 +1,111 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'berlin-clock',  
+  selector: 'berlin-clock',
   templateUrl: './berlin-clock.component.html',
   styleUrls: ['./berlin-clock.component.css']
 })
+
 export class BerlinClockComponent implements OnInit {
   berlinClock: { "lamps": { "light": number; }[]; }[];
-
+  
+  //79=Off
+  //82=Red
+  //89=Yellow
   ngOnInit(): void {
-    this.berlinClock = [{"lamps":[{"light":79}]},{"lamps":[{"light":82},{"light":82},{"light":82},{"light":79}]},{"lamps":[{"light":82},{"light":82},{"light":82},{"light":79}]},{"lamps":[{"light":89},{"light":89},{"light":82},{"light":89},{"light":89},{"light":82},{"light":89},{"light":89},{"light":79},{"light":79},{"light":79}]},{"lamps":[{"light":89},{"light":89},{"light":79},{"light":79}]}];
+    this.berlinClock = [
+      {
+        "lamps": [
+          {
+            "light": 79
+          }
+        ]
+      },
+      {
+        "lamps": [
+          {
+            "light": 82
+          },
+          {
+            "light": 82
+          },
+          {
+            "light": 82
+          },
+          {
+            "light": 79
+          }
+        ]
+      },
+      {
+        "lamps": [
+          {
+            "light": 82
+          },
+          {
+            "light": 82
+          },
+          {
+            "light": 82
+          },
+          {
+            "light": 79
+          }
+        ]
+      },
+      {
+        "lamps": [
+          {
+            "light": 89
+          },
+          {
+            "light": 89
+          },
+          {
+            "light": 82
+          },
+          {
+            "light": 89
+          },
+          {
+            "light": 89
+          },
+          {
+            "light": 82
+          },
+          {
+            "light": 89
+          },
+          {
+            "light": 89
+          },
+          {
+            "light": 79
+          },
+          {
+            "light": 79
+          },
+          {
+            "light": 79
+          }
+        ]
+      },
+      {
+        "lamps": [
+          {
+            "light": 89
+          },
+          {
+            "light": 89
+          },
+          {
+            "light": 79
+          },
+          {
+            "light": 79
+          }
+        ]
+      }
+    ];
   }
 }
