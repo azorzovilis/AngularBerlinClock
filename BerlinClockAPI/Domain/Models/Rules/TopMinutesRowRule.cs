b950beaa57bcc7cf1db2ec09edcd01c3c0df1794
azorzovilis <alexzorzov@gmail.com>
2020-03-22 00:20:28 +0000

    Simplified Top Minutes Rule

--- BerlinClockAPI/Domain/Models/Rules/TopMinutesRowRule.cs
+++ BerlinClockAPI/Domain/Models/Rules/TopMinutesRowRule.cs
@@ -5,12 +5,13 @@
 
     internal class TopMinutesRowRule : IRowRule
     {
-        private readonly Func<int, bool> _isThirdLamp = i => i == (3 - 1) || i == (6 - 1) || i == (9 - 1);
+        private readonly Func<int, bool> _isThirdLamp = index => index == (3 - 1) || index == (6 - 1) || index == (9 - 1);
+        private readonly Func<int, int, bool> _isSwitchedOn = (minutes, index) => minutes / (5 * (index + 1)) >= 1;
 
-        public Func<int, int, Lamp> Rule => 
-            (minutes, index) => 
-            minutes / (5 * (index + 1)) >= 1 
-                ? _isThirdLamp(index) ? new Lamp(LampLight.Red, true) : new Lamp(LampLight.Yellow, true) 
-                : _isThirdLamp(index) ? new Lamp(LampLight.Red, false) : new Lamp(LampLight.Yellow, false);
+        public Func<int, int, Lamp> Rule =>
+            (minutes, index) =>
+                _isThirdLamp(index)
+                    ? new Lamp(LampLight.Red, _isSwitchedOn(minutes, index))
+                    : new Lamp(LampLight.Yellow, _isSwitchedOn(minutes, index));
     }
 }
